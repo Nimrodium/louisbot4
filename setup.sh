be3d1aafb80026249f9c6128d2583cee5b853ad1
nimrod <nimrodium@protonmail.com>
2025-08-21 15:12:02 -0500

    updated setup.sh, dunno if it works tho

--- setup.sh
+++ setup.sh
@@ -14,5 +14,32 @@ fi
 pip install -r requirements.txt
 
 echo "cd $SCRIPT_DIR||exit;source $INSTALL_DIR/bin/activate;python $EXE" > run.sh
+
+service="[Unit]
+Description=Discord Server Statistics Bot
+After=network.target
+
+[Service]
+WorkingDirectory=${INSTALLDIR}
+ExecStart=${INSTALLDIR}/.venv/bin/python -u ${INSTALLDIR}/louisbot4.py
+User=$(whoami)
+
+#StandardOutput=append:${INSTALLDIR}/logs/stdout.log
+#StandardError=append:${INSTALLDIR}/logs/stderr.log
+[Install]
+WantedBy=multi-user.target
+" 
+
+
+read -r -p "Install Service (systemd)? [y/N] " response
+response=${response,,}    # tolower
+if [[ "$response" =~ ^(yes|y)$ ]]
+	path=/etc/systemd/system/louisbot.service
+	echo "Writing \n${service}\n---\n to ${path}"
+	echo ${service} > | sudo tee ${path}
+else
+	echo "generated service file:\n---\n${service}\n"
+fi
+
 chmod +x run.sh
 ./run.sh
